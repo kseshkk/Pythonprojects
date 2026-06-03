358fe5e1fec3b44fb7eea3f99cad29188d263594
kseshkk <ksesha658@gmail.com>
2026-06-03 16:17:43 +0300

    .

---
+++ LessonsPython/Homework/Homework20_SQL/task1.sql
@@ -0,0 +1,18 @@
+-- INSERT INTO products (name, category, price, quantity, discount_percent)
+-- VALUES 
+-- ('Ноутбук Lenovo', 'Ноутбуки', 55000, 4, 10),
+-- ('Мышка Logitech', 'Аксессуары', 2500, 15, 0),
+-- ('Клавиатура HyperX', 'Аксессуары', 7000, 8, 5),
+-- ('Монитор Samsung', 'Мониторы', 18000, 3, 15),
+-- ('Наушники Sony', 'Аудио', 12000, 6, 0)
+
+-- SELECT * FROM products
+-- SELECT name FROM products
+-- SELECT * FROM products WHERE price > 8000
+-- SELECT * FROM products WHERE quantity > 0
+-- SELECT * FROM products WHERE discount_percent > 0
+
+-- UPDATE products SET price=20000 WHERE id=4
+-- UPDATE products SET quantity= quantity-1 WHERE id=4
+
+-- DELETE FROM products WHERE quantity=0
