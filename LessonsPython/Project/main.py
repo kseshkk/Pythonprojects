ed24909cad2c8deb5fff3dacc5de5f759f631070
kseshkk <ksesha658@gmail.com>
2026-08-20 13:11:44 +0300

    /

--- LessonsPython/Project/main.py
+++
@@ -1,8 +0,0 @@
-# 8952728261:AAGjc_muoO8KYJ3e8TRdxzHRZ4MAwBzoHsU
-# @travl_hunter_bot
-
-import telebot
-
-bot = telebot.TeleBot("8952728261:AAGjc_muoO8KYJ3e8TRdxzHRZ4MAwBzoHsU")
-
-bot.infinity_polling()
