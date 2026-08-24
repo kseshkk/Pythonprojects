aaf3159dff2a772712a85abaca673b059be219fc
kseshkk <ksesha658@gmail.com>
2026-08-24 13:25:20 +0300

    /

--- LessonsPython/WalkHelper/main.py
+++ LessonsPython/WalkHelper/main.py
@@ -7,8 +7,10 @@ import ui.screen1_start.handlers
 import ui.screen2_main_menu.handlers
 import ui.screen3_walk_planning_step1.handlers
 import ui.screen4_walk_planning_step2.handlers
-
-
+import ui.screen5_walk_planning_step3.handlers
+import ui.screen6_walk_planning_step4.handlers
+import ui.screen7_show_planned_walks.handlers
+import ui.screen8_show_planned_walks.handlers
 
 
 bot.add_custom_filter(custom_filters.StateFilter(bot))
